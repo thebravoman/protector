52e91b02135246a309aefc9e9cfe40cf9010273d
Jason Kay <jason@callpixels.com>
2015-10-05 20:03:52 -0400

    Don't validate protections on Rails 4.1 HABTM join models.

--- lib/protector/adapters/active_record/validations.rb
+++ lib/protector/adapters/active_record/validations.rb
@@ -3,7 +3,8 @@ module Protector
     module ActiveRecord
       module Validations
         def valid?(*args)
-          if protector_subject?
+          #                        Don't validate protections on Rails 4.1 HABTM join models.
+          if protector_subject? && self.class.name !~ /\AHABTM_/
             state  = Protector.insecurely{ super(*args) }
             method = new_record? ? :first_uncreatable_field : :first_unupdatable_field
             field  = protector_meta.send(method, protector_changed)
