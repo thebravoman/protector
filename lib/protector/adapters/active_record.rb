48c63ece943f23505e2cc21f21ab95ba13f1315b
Kiril Mitov <kiril@retreaver.com>
2022-11-14 15:47:41 +0200

    [rails5] using ActiveSupport.on_load(:active_record) to load ActiveRecord
    Otherwise the ActiveRecord is loaded after config/application.rb but before config/initializers/new_framework_defaults_5_1.rb which leads to the configuration in
    new_framework_defaults not being taken into accout

--- lib/protector/adapters/active_record.rb
+++ lib/protector/adapters/active_record.rb
@@ -15,15 +15,18 @@ module Protector
       def self.activate!
         return false unless defined?(::ActiveRecord)
 
-        ::ActiveRecord::Base.send :include, Protector::Adapters::ActiveRecord::Base
-        ::ActiveRecord::Base.send :include, Protector::Adapters::ActiveRecord::Validations
-        ::ActiveRecord::Relation.send :include, Protector::Adapters::ActiveRecord::Relation
-        ::ActiveRecord::Associations::SingularAssociation.send :include, Protector::Adapters::ActiveRecord::Association
-        ::ActiveRecord::Associations::SingularAssociation.send :include, Protector::Adapters::ActiveRecord::SingularAssociation
-        ::ActiveRecord::Associations::CollectionAssociation.send :include, Protector::Adapters::ActiveRecord::Association
-        ::ActiveRecord::Associations::Preloader.send :include, Protector::Adapters::ActiveRecord::Preloader
-        ::ActiveRecord::Associations::Preloader::Association.send :include, Protector::Adapters::ActiveRecord::Preloader::Association
-        ::ActiveRecord::Associations::CollectionProxy.send :include, Protector::Adapters::ActiveRecord::CollectionProxy
+        ActiveSupport.on_load(:active_record) do
+          ::ActiveRecord::Base.send :include, Protector::Adapters::ActiveRecord::Base
+          ::ActiveRecord::Base.send :include, Protector::Adapters::ActiveRecord::Validations
+          ::ActiveRecord::Relation.send :include, Protector::Adapters::ActiveRecord::Relation
+          ::ActiveRecord::Associations::SingularAssociation.send :include, Protector::Adapters::ActiveRecord::Association
+          ::ActiveRecord::Associations::SingularAssociation.send :include, Protector::Adapters::ActiveRecord::SingularAssociation
+          ::ActiveRecord::Associations::CollectionAssociation.send :include, Protector::Adapters::ActiveRecord::Association
+          ::ActiveRecord::Associations::Preloader.send :include, Protector::Adapters::ActiveRecord::Preloader
+          ::ActiveRecord::Associations::Preloader::Association.send :include, Protector::Adapters::ActiveRecord::Preloader::Association
+          ::ActiveRecord::Associations::CollectionProxy.send :include, Protector::Adapters::ActiveRecord::CollectionProxy
+        end
+
       end
 
       def self.modern?
